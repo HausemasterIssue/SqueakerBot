fa659d265ca6a603ce783d4a202a6fe6433774a7
HausemasterIssue <90464553+HausemasterIssue@users.noreply.github.com>
2022-01-28 18:38:47 -0500

    Cleanup

---
+++ src/main/java/dev/hause/squeakerbot/util/ChatUtil.java
@@ -0,0 +1,14 @@
+package dev.hause.squeakerbot.util;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.play.client.CPacketChatMessage;
+
+public class ChatUtil {
+	
+	static Minecraft mc = Minecraft.getMinecraft();
+	
+	public static void sendChatMessage(String message) {
+		mc.player.connection.sendPacket(new CPacketChatMessage(message));
+	}
+
+}
